45465a9903e40e8f0774ba8920bbe50229355a44
a <a@psykolinux.lan>
2014-09-20 06:17:06 +0200

    qsdqs

--- main.c
+++ main.c
@@ -6,7 +6,23 @@
 /*   By: a <a@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/08/19 14:58:54 by elacombe          #+#    #+#             */
-/*   Updated: 2014/09/20 06:11:31 by a                ###   ########.fr       */
+/*   Updated: 2014/09/20 06:16:54 by a                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int main(int argc, char const *argv[])
+{
+	if (argc == 1)
+	{
+		/* traitement de l'entrée standard */
+		ft_stdin_handle();
+	}
+	return 0;
+}
+
+void	ft_stdin_handle()
+{
+	int		offset;
+	char	buffer[4069];
+	read ();
+}

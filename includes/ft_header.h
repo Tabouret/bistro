64576abda4f56b91c9daa62f876cbaffc45d74c4
a <a@psykolinux.lan>
2014-09-20 06:13:12 +0200

    sd

--- includes/ft_header.h
+++ includes/ft_header.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_header.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pollier <pollier@student.42.fr>            +#+  +:+       +#+        */
+/*   By: a <a@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/08/18 15:46:38 by elacombe          #+#    #+#             */
-/*   Updated: 2014/08/21 22:58:46 by pollier          ###   ########.fr       */
+/*   Updated: 2014/09/20 06:12:09 by a                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,22 +35,6 @@ typedef struct	s_params
 	int			pos;
 }				t_params;
 
-void			ft_open_file(char **argv, int i);
-void			ft_get_grid_param(int i);
-void			ft_read_files(char *filename);
-char			*ft_read_grid_first_line(char *str);
-int				ft_read_grid_second_line(char *str);
-char			*ft_strcat(char *dest, char *src);
-int				ft_atoi(char *s1);
 t_params		*ft_struct(int call);
-int				ft_strnlen(char *str, int offset);
-int				ft_strlen(char *str);
-void			ft_fuck_first_line(char *str);
-void			ft_parse(char *str);
-int				ft_jaylachiasse(int i);
-void			ft_courante(void);
-void			ft_ai_je_la_chiasse(int i);
-void			ft_chiasse_a_cour();
-void			ft_sauce_septique(void);
 
 #endif
